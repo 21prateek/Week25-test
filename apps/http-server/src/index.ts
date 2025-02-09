f7b4d6deb8f5499da69d82883d5a2171ee0269a5
21prateek <prateekgurung21@gmail.com>
2025-02-09 15:13:31 +0530

    Code commit

---
+++ apps/http-server/src/index.ts
@@ -0,0 +1,32 @@
+import express from "express";
+import { client } from "@repo/db/client";
+
+const app = express();
+const port = 3002;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.post("/signup", async (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  const user = await client.user.create({
+    data: {
+      username,
+      password,
+    },
+  });
+
+  res.json({
+    message: "Create a user",
+    id: user.id,
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
